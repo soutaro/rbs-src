4dfe99f38f3b0a8903db895d6f2bc5306f1279fe
Soutaro Matsumoto <matsumoto@soutaro.com>
2023-08-01 16:25:33 +0900

    Install all versions of the gem

--- lib/rbs/src/gem.rb
+++ lib/rbs/src/gem.rb
@@ -28,7 +28,7 @@ module Rbs
         runner.execute!("git", "clone", "--filter=blob:none", "--sparse", repository_url, repository_root.to_s)
         dirs = runner.query!("git", "ls-tree", "-d", "--name-only", "-z", "HEAD", chdir: repository_root).split("\0")
         dirs.delete(repository_dir.to_s)
-        runner.execute!("git", "sparse-checkout", "set", *dirs, repository_dir.join(name, version).to_s, chdir: repository_root)
+        runner.execute!("git", "sparse-checkout", "set", *dirs, repository_dir.join(name).to_s, chdir: repository_root)
         if commit
           runner.puts "git checkout..."
           runner.execute!("git", "checkout", commit, chdir: repository_root)
